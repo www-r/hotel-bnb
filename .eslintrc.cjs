db8e6a63310db2de61d74905da9fb2892286d584
dokimion24 <leecy3610@gmail.com>
2023-03-16 16:20:25 +0900

    Conf: eslintrc 설정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: ['import', 'prettier', 'react', 'react-hooks', 'jsx-a11y'], // 설치한 추가적인 규칙 설정
-  extends: ['airbnb', 'prettier'],
+  extends: ['prettier'],
   env: {
     browser: true, // 실행 환경에 대해서 등록
     node: true,
@@ -9,6 +9,6 @@ module.exports = {
   rules: {
     'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
   },
+
   parserOptions: { ecmaVersion: 2020 },
-  projectDependencies: false,
 }
