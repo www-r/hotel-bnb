10c684963a70fea15c9ef641d0d63a09534cea53
dokimion24 <leecy3610@gmail.com>
2023-03-18 00:22:30 +0900

    Design: AccountPage 스타일 컴포넌트 생성

---
+++ src/pages/AccountPage/AccountPage.style.jsx
@@ -0,0 +1,53 @@
+import styled from '@emotion/styled'
+
+export const Container = styled.main`
+  height: 100vh;
+`
+
+export const ContainerWrapper = styled.section`
+  margin: auto;
+  padding: 0 24px;
+`
+
+export const HeaderContainer = styled.div`
+  margin: 40px 0;
+`
+export const HeaderWrapper = styled.ul``
+export const HeaderLiWrapper = styled.li`
+  display: inline;
+`
+
+export const HeaderTitle = styled.h1`
+  font-size: 32px;
+  font-weight: 800;
+`
+
+export const menu = styled.div`
+  display: flex;
+  border-bottom: 1px solid;
+`
+
+export const ContentContainer = styled.div`
+  display: flex;
+`
+
+export const MainContainer = styled.div`
+  width: 60%;
+`
+
+export const SubContainer = styled.div`
+  border: 1px solid rgb(221, 221, 221);
+  border-radius: 12px;
+  padding: 24px;
+  width: 30%;
+  margin-left: 10%;
+`
+
+export const ContentTitle = styled.h2`
+  font-size: 22px;
+`
+
+export const Bold = styled.h3`
+  font-size: 16px;
+  font-weight: 600px;
+`
