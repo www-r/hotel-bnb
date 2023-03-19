64e0455cace88fe941bea534ad88d1354f4f268e
dokimion24 <leecy3610@gmail.com>
2023-03-19 16:16:26 +0900

    Feat: firebase 예시 코드 삭제

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,27 +1,7 @@
 import React from 'react'
-import { db } from '@/firebase'
-import { addDoc, collection, getDocs } from 'firebase/firestore'
 
 const MainPage = () => {
-  const setFunc = async () => {
-    const docRef = await addDoc(collection(db, 'adasd'), {
-      FirstName: 'wwww',
-      LastName: 'lawwwst',
-      age: 500,
-    })
-    console.log(docRef)
-  }
-
-  const getFunc = async () => {
-    const querySnapshot = await getDocs(collection(db, 'rooms'))
-    querySnapshot.forEach((data) => console.log(data.data()))
-  }
-  return (
-    <div>
-      <button onClick={setFunc}>set</button>
-      <button onClick={getFunc}>get</button>
-    </div>
-  )
+  return <>Main</>
 }
 
 export default MainPage
