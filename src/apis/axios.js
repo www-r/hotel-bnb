4c1b7c784fc84558427024db2c301fdfacd5bd4d
dokimion24 <leecy3610@gmail.com>
2023-03-19 16:19:03 +0900

    Feat: axiosIntance 삭제

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -1,14 +1,5 @@
 import axios from 'axios'
 
-export const axiosInstance = axios.create({
-  baseURL: `${import.meta.env.VITE_BASE_URL}`,
-  headers: {
-    'content-type': 'application/json',
-    apikey: `${import.meta.env.VITE_API_KEY}`,
-    username: `${import.meta.env.VITE_USERNAME}`,
-  },
-})
-
 export const axiosFirebase = axios.create({
   baseURL: `${import.meta.env.VITE_FB_BASE_URL}`,
 })
