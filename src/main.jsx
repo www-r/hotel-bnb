9481f01af0c4ac87d73f486ef8426a727b7c3c02
dokimion24 <leecy3610@gmail.com>
2023-03-21 21:48:10 +0900

    Fix: import 수정 global -> Global

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
-import GlobalStyle from '@/styles/global'
+import GlobalStyle from '@/styles/Global'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
