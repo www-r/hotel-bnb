1d8f9c6179606557fc143e01d62cc79ff1ffb1a1
dokimion24 <leecy3610@gmail.com>
2023-03-21 16:08:37 +0900

    Feat: Heart.svg  index.js 추가

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,4 +1,5 @@
 import IconHeaderMenu from './IconHeaderMenu.svg'
 import IconHeaderAuthor from './IconHeaderAuthor.svg'
+import { ReactComponent as HeartEmpty } from '@/assets/images/HeartEmpty.svg'
 
-export { IconHeaderMenu, IconHeaderAuthor }
+export { IconHeaderMenu, IconHeaderAuthor, HeartEmpty }
