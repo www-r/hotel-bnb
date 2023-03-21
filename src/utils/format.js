e93400efa342d237e17b27911c2663b63f968977
dokimion24 <leecy3610@gmail.com>
2023-03-21 19:38:57 +0900

    Feat: formatDate 함수 수정

--- src/utils/format.js
+++ src/utils/format.js
@@ -1,5 +1,14 @@
 export const formatDate = (date) => {
-  return `${date.slice(5, 7)}월 ${date.slice(8, 10)}일`
+  let reservStart = new Date()
+  let reservEnd = new Date()
+
+  reservStart = new Date(reservStart.setDate(reservStart.getDate() - date.start))
+  reservEnd = new Date(reservEnd.setDate(reservEnd.getDate() + date.end))
+
+  return `${String(reservStart.getMonth() + 1).padStart(2, '0')}월 ${String(reservStart.getDate()).padStart(
+    2,
+    '0',
+  )}일 ~  ${String(reservEnd.getMonth() + 1).padStart(2, '0')}월 ${String(reservEnd.getDate())}일`
 }
 
 export const formatPrice = (price) => {
