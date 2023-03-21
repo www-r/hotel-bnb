a510bddd5a1615764867ad30138637c8f8f56467
dokimion24 <leecy3610@gmail.com>
2023-03-21 14:17:59 +0900

    Design: MapButton 컴포넌트 스타일 지정

---
+++ src/components/Map/MapButton/MapButton.style.jsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled'
+
+export const Button = styled.button`
+  position: fixed;
+  left: 50%;
+  transform: translateX(-50%);
+  bottom: 50px;
+
+  background-color: #222;
+  border-radius: 26px;
+  width: 160px;
+  height: 50px;
+  color: #fff;
+  font-weight: 600;
+`
