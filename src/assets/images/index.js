a475495e5b113e7e0b55d22e04a1ce7639955624
bwlee <lbw3973@naver.com>
2023-03-21 19:47:34 +0900

    Feat: Image 추가

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,5 +1,6 @@
 import IconHeaderMenu from './IconHeaderMenu.svg'
 import IconHeaderAuthor from './IconHeaderAuthor.svg'
 import { ReactComponent as Heart } from '@/assets/images/Heart.svg'
+import { ReactComponent as IconExit } from './IconExit.svg'
 
-export { IconHeaderMenu, IconHeaderAuthor, Heart }
+export { IconHeaderMenu, IconHeaderAuthor, IconExit, Heart }
