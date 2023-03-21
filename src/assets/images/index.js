06b4d0b2f03c14e549641a674544831b329fbe1c
youngeun <jessicak0828@gmail.com>
2023-03-21 21:33:42 +0900

    feat: 아이콘 추가

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,5 +1,6 @@
 import IconHeaderMenu from './IconHeaderMenu.svg'
 import IconHeaderAuthor from './IconHeaderAuthor.svg'
-import { ReactComponent as Heart } from '@/assets/images/Heart.svg'
 
-export { IconHeaderMenu, IconHeaderAuthor, Heart }
+import { ReactComponent as Heart } from '@/assets/images/Heart.svg'
+import { ReactComponent as ChevronRight } from '@/assets/images/ChevronRight.svg'
+export { IconHeaderMenu, IconHeaderAuthor, Heart, ChevronRight }
