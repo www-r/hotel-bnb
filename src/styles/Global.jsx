f6f47b2626b43f0ea8d3cfe5767d758bfe19bda0
bwlee <lbw3973@naver.com>
2023-03-24 00:11:46 +0900

    Design: Global Style 수정

--- src/styles/Global.jsx
+++ src/styles/Global.jsx
@@ -99,9 +99,9 @@ const style = css`
   }
 
   * {
-    font-family: 'Pretendard Variable', Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto,
-      'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', 'Apple Color Emoji',
-      'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif;
+    font-family: 'Pretendard Variable', Pretendard, -apple-system, BlinkMacSystemFont, system-ui,
+      Roboto, 'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic',
+      'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif;
   }
 
   button {
