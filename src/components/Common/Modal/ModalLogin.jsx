46daeea80a8ea407463c2b69ba6812742a46273b
bwlee <lbw3973@naver.com>
2023-03-23 12:57:49 +0900

    Feat: 로그인 기능 구현 완료

--- src/components/Common/Modal/ModalLogin.jsx
+++ src/components/Common/Modal/ModalLogin.jsx
@@ -1,9 +1,35 @@
-import React, { useState } from 'react'
-import { IconExit } from '../../../assets/images'
+import React, { useEffect, useState } from 'react'
+import { GoogleFavicon, IconExit } from '../../../assets/images'
 import * as S from './ModalLogin.style'
+import { app, loginEmail, loginGoogle } from '../../../firebase'
+import { getAuth } from 'firebase/auth'
 
 const ModalLogin = (props) => {
   const { open, set } = props
+  const [values, setValues] = useState({ ID: '', PW: '' })
+
+  const handleChange = (e) => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  const login = (e) => {
+    e.preventDefault()
+    loginEmail(values.ID, values.PW).then((result) => {
+      console.log(result)
+      // const user = result.user
+    })
+  }
+
+  const googleLogin = () => {
+    loginGoogle().then((result) => {
+      console.log(result)
+      // const user = result.user
+    })
+  }
+
   return (
     <S.Container open={open}>
       <S.LoginContainer>
@@ -23,17 +49,39 @@ const ModalLogin = (props) => {
           <S.TextContainer>
             <h1>호텔비엔비에 오신 것을 환영합니다.</h1>
           </S.TextContainer>
-          <S.LoginForm>
+          <S.LoginForm onSubmit={login}>
             <S.InputContainer upper={true}>
-              <div>
-                <p>ID</p>
-              </div>
-              <S.InputIDPW />
+              <S.ParagraphDiv>
+                <p>아이디</p>
+              </S.ParagraphDiv>
+              <S.InputIDPW name="ID" value={values.ID} onChange={handleChange} />
             </S.InputContainer>
             <S.InputContainer upper={false}>
-              <S.InputIDPW />
+              <S.ParagraphDiv>
+                <p>비밀번호</p>
+              </S.ParagraphDiv>
+              <S.InputIDPW type={'password'} name="PW" value={values.PW} onChange={handleChange} />
             </S.InputContainer>
+            <S.SpanLoginConfirm></S.SpanLoginConfirm>
+            <S.TextContainer>
+              <S.BtnSubmit type="submit">로그인</S.BtnSubmit>
+            </S.TextContainer>
           </S.LoginForm>
+          <S.TextContainer>
+            <S.ImageContainer>
+              <GoogleFavicon />
+            </S.ImageContainer>
+            <S.BtnGoogleLogin onClick={googleLogin}>구글로 로그인하기</S.BtnGoogleLogin>
+          </S.TextContainer>
+          <S.TextContainer>
+            <S.BtnGoogleLogin
+              onClick={() => {
+                console.log(getAuth(app).currentUser)
+              }}
+            >
+              현재 유저 확인
+            </S.BtnGoogleLogin>
+          </S.TextContainer>
         </S.ContentContainer>
       </S.LoginContainer>
     </S.Container>
--- src/components/Common/Modal/ModalLogin.style.jsx
+++ src/components/Common/Modal/ModalLogin.style.jsx
@@ -43,21 +43,35 @@ export const TextContainer = styled.div`
   margin-top: 8px;
   margin-bottom: 24px;
   font-size: 22px;
+  position: relative;
 `
 
 export const LoginForm = styled.form``
 
 export const InputContainer = styled.div`
+  position: relative;
   display: flex;
   flex-direction: column;
   border: 1px solid;
   ${(props) => (props.upper ? '' : `border-top: none;`)}
   border-radius: ${(props) => (props.upper ? '8px 8px 0 0;' : '0 0 8px 8px')};
+  &:focus-within {
+    border: 2px solid;
+  }
+`
+
+export const ParagraphDiv = styled.div`
+  position: absolute;
+  top: 2px;
+  left: 12px;
+  right: 12px;
+  padding: 0 24px 0 0;
+  font-weight: bold;
 `
 
 export const InputIDPW = styled.input`
-  font-size: 16px;
-  padding: 26px 36px 10px 12px;
+  font-size: 20px;
+  padding: 36px 36px 20px 12px;
   border-radius: 8px;
   border: none;
   &:focus {
@@ -73,4 +87,30 @@ export const BtnCloseModal = styled.button`
   bottom: ${(props) => props.bottom}px;
   background-color: inherit;
   cursor: pointer;
+  border: none;
+`
+
+export const SpanLoginConfirm = styled.span``
+
+export const BtnSubmit = styled.button`
+  background-color: purple;
+  border: 1px solid;
+  width: 100%;
+  border-radius: 8px;
+  padding: 14px;
+  color: #fff;
+  font-size: 16px;
+  font-weight: bold;
+`
+
+export const BtnGoogleLogin = styled(BtnSubmit)`
+  background-color: #fff;
+  color: #000;
+  font-size: 14px;
+`
+
+export const ImageContainer = styled.div`
+  position: absolute;
+  top: 15px;
+  left: 30px;
 `
