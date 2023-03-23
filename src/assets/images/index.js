c87fa87b51cdba654e221fd91e4f7e4f42b6c155
bwlee <lbw3973@naver.com>
2023-03-23 12:54:29 +0900

    Feat: Image 추가

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -2,5 +2,6 @@ import IconHeaderMenu from './IconHeaderMenu.svg'
 import IconHeaderAuthor from './IconHeaderAuthor.svg'
 import { ReactComponent as Heart } from '@/assets/images/Heart.svg'
 import { ReactComponent as IconExit } from './IconExit.svg'
+import { ReactComponent as GoogleFavicon } from './GoogleFavicon.svg'
 
-export { IconHeaderMenu, IconHeaderAuthor, IconExit, Heart }
+export { IconHeaderMenu, IconHeaderAuthor, IconExit, Heart, GoogleFavicon }
