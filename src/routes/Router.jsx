17bb1f27a7d704f3f029d15abfc800faf8ae0367
dokimion24 <leecy3610@gmail.com>
2023-03-23 21:04:11 +0900

    Feat: ReservationDetailPage -> PaymentPage 이름 변경

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -3,12 +3,12 @@ import { Route, Routes } from 'react-router-dom'
 import MapPage from '@/pages/MapPage/MapPage'
 import ProductDetailPage from '@/pages/RoomDetailPage/RoomDetailPage'
 import MainPage from '@/pages/MainPage/Mainpage'
-import ReservationDetailPage from '@/pages/ReservationDetailPage/ReservationDetailPage'
 import WishListPage from '@/pages/WishListpage/WishListpage'
 import PersonalInfoPage from '@/pages/PersonalInfoPage/PersonalInfoPage'
 import AccountPage from '@/pages/AccountPage/AccountPage'
 import ErrorPage from '@/pages/ErrorPage/ErrorPage'
 import AdminPage from '../pages/AdminPage/AdminPage'
+import PaymentPage from '../pages/PaymentPage/PaymentPage'
 
 const Router = () => {
   return (
@@ -16,7 +16,7 @@ const Router = () => {
       <Route path="/" element={<MainPage />} />
       <Route path="/room/:id" element={<ProductDetailPage />} />
       <Route path="/map" element={<MapPage />} />
-      <Route path="/book/:id" element={<ReservationDetailPage />} />
+      <Route path="/book/:id" element={<PaymentPage />} />
       <Route path="/wishlist" element={<WishListPage />} />
       <Route path="/personalInfo" element={<PersonalInfoPage />} />
       <Route path="/account" element={<AccountPage />} />
