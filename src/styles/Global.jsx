0167ca3089cec74ff91e17b519e5f0f4e27d9916
youngeun <jessicak0828@gmail.com>
2023-03-31 16:52:31 +0900

    fix: Change selector * to body

--- src/styles/Global.jsx
+++ src/styles/Global.jsx
@@ -99,10 +99,10 @@ const style = css`
     --border: 1px solid #ddd;
   }
 
-  * {
-    font-family: 'Pretendard Variable', Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto,
-      'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', 'Apple Color Emoji',
-      'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif;
+  body {
+    font-family: 'Pretendard Variable', Pretendard, -apple-system, BlinkMacSystemFont, system-ui,
+      Roboto, 'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic',
+      'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif;
   }
 
   button {
