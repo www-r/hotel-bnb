bbb9e5e8278e595b10d8bedd5a67a349ca3905a2
dokimion24 <leecy3610@gmail.com>
2023-04-01 16:05:05 +0900

    Feat: MainPage 컴포넌트에서 rooms 데이터 불러오기

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
+import { RoomsContext } from '@/contexts/RoomsProvider'
+
 import RoomList from '@/components/Room/RoomList/RoomList'
 import Header from '@/components/Common/Header/Header'
 import MapButton from '@/components/Map/MapButton/MapButton'
@@ -6,10 +8,13 @@ import MapButton from '@/components/Map/MapButton/MapButton'
 const MainPage = () => {
   const [movePage, setMovePage] = useState('/map')
 
+  const roomsCtx = useContext(RoomsContext)
+  const { rooms, loading, error } = roomsCtx
+
   return (
     <>
       <Header />
-      <RoomList />
+      <RoomList rooms={rooms} loading={loading} error={error} />
       <MapButton movePage={movePage} />
     </>
   )
