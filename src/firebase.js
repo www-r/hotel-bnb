d11213b944d27bb786baea1ff75c6bd85f95b623
bwlee <lbw3973@naver.com>
2023-04-01 23:23:35 +0900

    Feat: user정보 set,get provider로 이동

--- src/firebase.js
+++ src/firebase.js
@@ -132,22 +132,6 @@ export const CreateUser = async (email, password) => {
   }
 }
 
-// export const setCurrentUser = (setUser = null) => {
-//   const auth = getAuth()
-//   onAuthStateChanged(auth, (user) => {
-//     if (setUser) {
-//       if (user) {
-//         setUser(user)
-//       } else {
-//         setUser(null)
-//       }
-//     }
-//   })
-// }
-
-export const getCurrentUser = () => {
-  return getAuth().currentUser
-}
 // const auth = getAuth()
 // const user = auth.currentUser
 // const newPassword = getASecureRandomPassword()
