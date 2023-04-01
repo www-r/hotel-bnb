8d0cd9d89becdf5404d5e1756517407839219d97
LeeByoungWook <lbw3973@naver.com>
2023-04-01 16:59:32 +0900

    Feat: 회원가입, 구글로그인 시 email 정보 추가

--- src/components/Common/Modal/ModalSignUp.jsx
+++ src/components/Common/Modal/ModalSignUp.jsx
@@ -5,7 +5,7 @@ import { CreateUser, AddUserData } from '../../../firebase'
 
 const ModalSignUp = (props) => {
   const { open, set } = props
-  const [values, setValues] = useState({ ID: '', PW: '', NAME: '', PHONENUMBER: '' })
+  const [values, setValues] = useState({ EMAIL: '', PW: '', NAME: '', PHONENUMBER: '' })
 
   const handleChange = (e) => {
     setValues({
@@ -17,12 +17,12 @@ const ModalSignUp = (props) => {
   const signUp = async (e) => {
     e.preventDefault()
     let user
-    await CreateUser(values.ID, values.PW).then((result) => {
+    await CreateUser(values.EMAIL, values.PW).then((result) => {
       user = result
       console.log(result)
     })
     console.log(user)
-    await AddUserData(user.uid, values.NAME, values.PHONENUMBER)
+    await AddUserData(user.uid, values.EMAIL, values.NAME, values.PHONENUMBER)
     set(false)
   }
 
@@ -46,15 +46,24 @@ const ModalSignUp = (props) => {
             <S.SignUpForm onSubmit={signUp}>
               <S.InputDiv>
                 아이디
-                <S.InputUserInfo name="ID" value={values.ID} onChange={handleChange} />
+                <S.InputUserInfo name="EMAIL" value={values.EMAIL} onChange={handleChange} />
                 비밀번호
-                <S.InputUserInfo type={'password'} name="PW" value={values.PW} onChange={handleChange} />
+                <S.InputUserInfo
+                  type={'password'}
+                  name="PW"
+                  value={values.PW}
+                  onChange={handleChange}
+                />
                 비밀번화 확인
                 <S.InputUserInfo />
                 이름
                 <S.InputUserInfo name="NAME" value={values.NAME} onChange={handleChange} />
                 전화번호
-                <S.InputUserInfo name="PHONENUMBER" value={values.PHONENUMBER} onChange={handleChange} />
+                <S.InputUserInfo
+                  name="PHONENUMBER"
+                  value={values.PHONENUMBER}
+                  onChange={handleChange}
+                />
               </S.InputDiv>
               <button type={'submit'}>가입하기</button>
             </S.SignUpForm>
