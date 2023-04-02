ee338d2c4950756f2994b46ece0d33fac147fe4f
dokimion24 <leecy3610@gmail.com>
2023-04-02 16:06:57 +0900

    Feat: UserContext 사용

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -4,12 +4,16 @@ import { RoomsContext } from '@/contexts/RoomsProvider'
 import RoomList from '@/components/Room/RoomList/RoomList'
 import Header from '@/components/Common/Header/Header'
 import MapButton from '@/components/Map/MapButton/MapButton'
+import { UserContext } from '@/contexts/UserProvider'
 
 const MainPage = () => {
+  const user = useContext(UserContext)
+  // console.log('mainpage user', user)
   const [movePage, setMovePage] = useState('/map')
 
   const roomsCtx = useContext(RoomsContext)
   const { rooms, loading, error } = roomsCtx
+  // console.log('rooms', rooms)
 
   return (
     <>
