cd6474792a1838f6126ecdf1c202c8302b3afeee
dokimion24 <leecy3610@gmail.com>
2023-04-04 22:07:52 +0900

    Feat: 룸데이터 가져오는 roomContext 상위 컴포넌트 mainpage로 이동

--- src/components/Room/RoomList/RoomList.jsx
+++ src/components/Room/RoomList/RoomList.jsx
@@ -1,19 +1,12 @@
 import React, { useContext } from 'react'
 import RoomItem from '@/components/Room/RoomItem/RoomItem'
-import { RoomsContext } from '@/contexts/RoomsProvider'
 import { LoginContext } from '@/contexts/LoginProvider'
 
 import * as S from '@/components/Room/RoomList/RoomList.style'
 
-const RoomList = () => {
+const RoomList = ({ rooms }) => {
   const currentUser = useContext(LoginContext)
   console.log('currentUser', currentUser)
-
-  const roomsCtx = useContext(RoomsContext)
-  const { rooms, loading, error } = roomsCtx
-
-  if (loading) return <div>loading...</div>
-  if (error) return <div>에러가 발생</div>
 
   return (
     <S.Main>
