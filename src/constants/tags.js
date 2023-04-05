3991a9350f62c38a6b57d5c6340c4c0d10365569
bwlee <lbw3973@naver.com>
2023-04-05 20:33:08 +0900

    Feat: tagData.js 추가

---
+++ src/constants/tags.js
@@ -0,0 +1,67 @@
+export const TagsData = [
+  {
+    img: 'src/assets/images/TagIcons/Farm.jpg',
+    text: '농장',
+  },
+  {
+    img: 'src/assets/images/TagIcons/Beach.jpg',
+    text: '해변 바로앞',
+  },
+  {
+    img: 'src/assets/images/TagIcons/CountrySide.jpg',
+    text: '한적한 시골',
+  },
+  {
+    img: 'src/assets/images/TagIcons/PrivateRoom.jpg',
+    text: '개인실',
+  },
+  {
+    img: 'src/assets/images/TagIcons/GreatestView.jpg',
+    text: '최고의 전망',
+  },
+  {
+    img: 'src/assets/images/TagIcons/Amazing.jpg',
+    text: '기상천외한 숙소',
+  },
+  {
+    img: 'src/assets/images/TagIcons/KoreanHouse.jpg',
+    text: '한옥',
+  },
+  {
+    img: 'src/assets/images/TagIcons/Hottest.jpg',
+    text: '인기 급상승',
+  },
+  {
+    img: 'src/assets/images/TagIcons/SwimmingPool.jpg',
+    text: '멋진 수영장',
+  },
+  ,
+  {
+    img: 'src/assets/images/TagIcons/Camping.jpg',
+    text: '캠핑',
+  },
+  {
+    img: 'src/assets/images/TagIcons/Ryokan.jpg',
+    text: '료칸',
+  },
+  {
+    img: 'src/assets/images/TagIcons/Tongnamoo.jpg',
+    text: '통나무집',
+  },
+  {
+    img: 'src/assets/images/TagIcons/CampingCar.jpg',
+    text: '캠핑카',
+  },
+  {
+    img: 'src/assets/images/TagIcons/CreatePlace.jpg',
+    text: '창작 공간',
+  },
+  {
+    img: 'src/assets/images/TagIcons/DomeHouse.jpg',
+    text: '돔하우스',
+  },
+  {
+    img: 'src/assets/images/TagIcons/Kitchen.jpg',
+    text: '전문가급 주방',
+  },
+]
