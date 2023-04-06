bb216cd8e017a2e3b8b961ac55b5b7b777ba47a0
dokimion24 <leecy3610@gmail.com>
2023-04-06 20:23:29 +0900

    Feat: map 스타일 추가

--- src/pages/MapPage/MapPage.jsx
+++ src/pages/MapPage/MapPage.jsx
@@ -3,7 +3,9 @@ import { RoomsContext } from '@/contexts/RoomsProvider'
 
 import MapList from '@/components/Map/MapList/MapList'
 import Header from '@/components/Common/Header/Header'
-import MapButton from '../../components/Map/MapButton/MapButton'
+import MapButton from '@/components/Map/MapButton/MapButton'
+
+import '@/components/Map/map.css'
 
 const MapPage = () => {
   const roomsCtx = useContext(RoomsContext)
