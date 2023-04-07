fad6335d6c36e3083215247a33fa88a5530f254a
bwlee <lbw3973@naver.com>
2023-04-07 20:50:58 +0900

    Feat: 지역으로 검색하기 레이아웃 제작 및 이미지 추가

--- src/components/Common/Modal/ModalSearchByLocation.jsx
+++ src/components/Common/Modal/ModalSearchByLocation.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 import * as S from './ModalSearchByLocation.style'
 
 const ModalSearchByLocation = ({ show }) => {
-  return <S.Container show={show}>ModalSearchByLocation</S.Container>
+  return (
+    <S.Container show={show}>
+      <S.TitleContainer>지역으로 검색하기</S.TitleContainer>
+      <S.Wrapper>
+        <S.LocationImage src="/src/assets/images/SearchLocation/Gyeonggi.png" />
+        <S.LocationImage src="/src/assets/images/SearchLocation/Gangwon.png" />
+        <S.LocationImage src="/src/assets/images/SearchLocation/Jeju.png" />
+        <S.LocationImage src="/src/assets/images/SearchLocation/North_Chungcheong.png" />
+        <S.LocationImage src="/src/assets/images/SearchLocation/North_Jeolla.png" />
+        <S.LocationImage src="/src/assets/images/SearchLocation/South_Gyeongsang.png" />
+      </S.Wrapper>
+    </S.Container>
+  )
 }
 
 export default ModalSearchByLocation
