cfefb6aae9480493d20f8a0a7d2e48341570df6d
LeeByoungWook <lbw3973@naver.com>
2023-04-09 04:00:19 +0900

    Feat: 지도 image에 name속성 추가

--- src/components/Common/Modal/ModalSearchByLocation.jsx
+++ src/components/Common/Modal/ModalSearchByLocation.jsx
@@ -10,7 +10,7 @@ const ModalSearchByLocation = ({ show, click }) => {
   const handleClick = (e) => {
     if (e.target.tagName === 'IMG') {
       const res = rooms.filter((room) => room.location.includes(e.target.alt))
-      navigate(`/search/${e.target.alt}`, {
+      navigate(`/search/${e.target.name}`, {
         state: { res },
       })
       click(false)
@@ -20,17 +20,35 @@ const ModalSearchByLocation = ({ show, click }) => {
     <S.Container show={show}>
       <S.TitleContainer>지역으로 검색하기</S.TitleContainer>
       <S.Wrapper onClick={handleClick}>
-        <S.LocationImage src="/src/assets/images/SearchLocation/Gyeonggi.png" alt="경기도" />
-        <S.LocationImage src="/src/assets/images/SearchLocation/Gangwon.png" alt="강원도" />
-        <S.LocationImage src="/src/assets/images/SearchLocation/Jeju.png" alt="제주도" />
+        <S.LocationImage
+          src="/src/assets/images/SearchLocation/Gyeonggi.png"
+          alt="경기도"
+          name="Gyeonggi"
+        />
+        <S.LocationImage
+          src="/src/assets/images/SearchLocation/Gangwon.png"
+          alt="강원도"
+          name="Gangwon"
+        />
+        <S.LocationImage
+          src="/src/assets/images/SearchLocation/Jeju.png"
+          alt="제주도"
+          name="Jeju"
+        />
         <S.LocationImage
           src="/src/assets/images/SearchLocation/North_Chungcheong.png"
           alt="충청북도"
+          name="North_Chungcheong"
         />
-        <S.LocationImage src="/src/assets/images/SearchLocation/North_Jeolla.png" alt="전라북도" />
+        <S.LocationImage
+          src="/src/assets/images/SearchLocation/North_Jeolla.png"
+          alt="전라북도"
+          name="North_Jeolla"
+        />
         <S.LocationImage
           src="/src/assets/images/SearchLocation/South_Gyeongsang.png"
           alt="경상남도"
+          name="South_Gyeongsang"
         />
       </S.Wrapper>
     </S.Container>
