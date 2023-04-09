86332ad9886416735b26775cc53f9f226cec7c1a
dokimion24 <leecy3610@gmail.com>
2023-04-09 11:44:42 +0900

    Feat: reservation 데이터 firebase 저장

--- src/components/Payment/PaymentWaiting.jsx
+++ src/components/Payment/PaymentWaiting.jsx
@@ -1,25 +1,34 @@
 import React, { useContext, useEffect } from 'react'
 import usePayApproval from '@/hooks/usePayApproval'
 import { LoginContext } from '@/contexts/LoginProvider'
+import { UserContext } from '@/contexts/UserProvider'
 import PayRedirectPage from '@/pages/PayRedirectPage/PayRedirectPage'
-import { useNavigate } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
+import usePostUserInfo from '@/hooks/usePostUserInfo'
 
 const props = {
   title: '결제 진행중...',
 }
 
 const PaymentWaiting = () => {
-  console.log('render')
+  console.log('.......render')
   const currentUser = useContext(LoginContext)
+  const userCtx = useContext(UserContext)
+
+  const location = useLocation()
+  const { postUserInfo } = usePostUserInfo()
+
   const navigate = useNavigate()
   const { postKaKaoPayApproval, data } = usePayApproval()
   const params = new URLSearchParams(location.search)
   const pg_token = params.get('pg_token')
   const tid = JSON.parse(localStorage.getItem(`tid`))
+  const room = JSON.parse(localStorage.getItem(`paymentRoom`))
+  // console.log(room)
 
-  console.log('tid', tid)
-  console.log('wait data', data)
-  console.log('pg_token', pg_token)
+  // console.log('tid', tid)
+  // console.log('wait data', data)
+  // console.log('pg_token', pg_token)
 
   const kakaoPayData = {
     cid: 'TC0ONETIME',
@@ -29,9 +38,22 @@ const PaymentWaiting = () => {
     pg_token,
   }
 
+  const reservation = {
+    title: room.title,
+    price: room.price,
+    reservationDays: '날짜',
+  }
+
   useEffect(() => {
+    userCtx.addItemToReservation(reservation)
     postKaKaoPayApproval(kakaoPayData)
   }, [])
+
+  useEffect(() => {
+    if (currentUser) {
+      postUserInfo(currentUser?.uid, userCtx.userDetailInfo)
+    }
+  }, [userCtx.userDetailInfo.reservations, currentUser])
 
   useEffect(() => {
     if (data) {
@@ -43,7 +65,7 @@ const PaymentWaiting = () => {
 
   return (
     <>
-      <PayRedirectPage {...props} />;
+      <PayRedirectPage {...props} />
     </>
   )
 }
