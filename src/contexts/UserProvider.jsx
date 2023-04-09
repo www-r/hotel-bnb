122dc13eb63335e853ca758e559aac601cc7156d
dokimion24 <leecy3610@gmail.com>
2023-04-09 11:43:52 +0900

    Feat: context userDetailInfo 객체 추가, reducer 변경

--- src/contexts/UserProvider.jsx
+++ src/contexts/UserProvider.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useReducer, useEffect } from 'react'
+import { createContext, useContext, useReducer, useEffect, useState } from 'react'
 import useGetData from '@/hooks/useGetData'
 import { LoginContext } from '@/contexts/LoginProvider'
 
@@ -7,47 +7,69 @@ export const UserContext = createContext({
   name: '',
   phoneNumber: '',
   profileImageURL: '',
-  reservations: [],
-  wishLists: [],
+  userDetailInfo: {
+    reservations: [],
+    wishLists: [],
+  },
   addItemToWish: (item) => {},
   deleteItemToWish: (item) => {},
+  addItemToReservation: (item) => {},
 })
 
-const initalWishState = []
+const initalState = { reservations: [], wishLists: [] }
 
-const wishReducer = (state, action) => {
+const userDetailInfoReducer = (state, action) => {
+  console.log('state', state, 'action', action)
   switch (action.type) {
     case 'ADD_WISH':
-      return [...state, action.payload]
+      return { ...state, wishLists: [...state.wishLists, action.payload.wishLists] }
     case 'DELETE_WISH':
-      return state.filter((item) => item.id !== action.payload.id)
+      return {
+        ...state,
+        wishLists: state.wishLists.filter((item) => item.id !== action.payload.wishLists.id),
+      }
     case 'INIT_WISH':
       return action.payload
+    case 'ADD_RESERVATION':
+      return { ...state, reservations: [...state.reservations, action.payload.reservations] }
   }
 }
 
 const UserProvider = ({ children }) => {
   const currentUser = useContext(LoginContext)
-  const [wishState, dispatchWishAction] = useReducer(wishReducer, initalWishState)
+  // const [reservationState, setReservationState] = useState({
+  //   title: null,
+  //   price: 0,
+  //   reservationDays: 0,
+  // })
+  const [userDetailState, dispatchUserDetailInfoAction] = useReducer(
+    userDetailInfoReducer,
+    initalState,
+  )
   // console.log('currentUser', currentUser)
   // 현재 로그인된 유저 정보 가져옴
   const { data: user } = useGetData(`/users/${currentUser?.uid}`)
 
   // 로그인된 유저의 wishList를 가져와 렌더
   useEffect(() => {
-    if (user?.wishLists) {
-      dispatchWishAction({ type: 'INIT_WISH', payload: user.wishLists })
+    if (user?.userDetailInfo?.wishLists) {
+      // console.log('userDetailInfo', user.userDetailInfo)
+      dispatchUserDetailInfoAction({ type: 'INIT_WISH', payload: user.userDetailInfo })
     }
-  }, [user?.wishLists])
+  }, [user?.userDetailInfo?.wishLists])
 
   // 업데이트 될때마다 firebase에 저장
 
   const addItemToWishHandler = (item) => {
-    dispatchWishAction({ type: 'ADD_WISH', payload: item })
+    dispatchUserDetailInfoAction({ type: 'ADD_WISH', payload: { wishLists: item } })
   }
 
   const deleteItemToWishHandler = (item) => {
-    dispatchWishAction({ type: 'DELETE_WISH', payload: item })
+    dispatchUserDetailInfoAction({ type: 'DELETE_WISH', payload: { wishLists: item } })
+  }
+
+  const addItemToReservationHandler = (item) => {
+    dispatchUserDetailInfoAction({ type: 'ADD_RESERVATION', payload: { reservations: item } })
   }
 
   const userContext = {
@@ -55,13 +77,14 @@ const UserProvider = ({ children }) => {
     name: user?.name,
     phoneNumber: user?.phoneNumber,
     profileImageURL: user?.profileImageURL,
-    reservations: [],
-    wishLists: wishState,
+    userDetailInfo: {
+      reservations: userDetailState.reservations,
+      wishLists: userDetailState.wishLists,
+    },
     addItemToWish: addItemToWishHandler,
     deleteItemToWish: deleteItemToWishHandler,
+    addItemToReservation: addItemToReservationHandler,
   }
-
-  // console.log('wishState', wishState)
 
   // usePostUserInfo(`${currentUser?.uid}`, userContext.wishLists)
 
