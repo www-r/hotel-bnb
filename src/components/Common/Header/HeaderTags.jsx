f9beee6bff2bfb5f78a60162a18b2c81180e7d99
bwlee <lbw3973@naver.com>
2023-04-11 22:57:57 +0900

    Feat: TagList에 react-slick slider 적용

--- src/components/Common/Header/HeaderTags.jsx
+++ src/components/Common/Header/HeaderTags.jsx
@@ -1,15 +1,32 @@
 import React from 'react'
 import HeaderTagItem from './HeaderTagItem'
 import { TagsData } from '../../../constants/tags'
+import Slider from 'react-slick'
+import styled from 'styled-components'
+import './slick.css'
+import './slick-theme.css'
 
 const HeaderTags = () => {
   return (
     <>
-      {TagsData.map((tag) => (
-        <HeaderTagItem key={tag.text} text={tag.text} img={tag.img} />
-      ))}
+      <StyledSlider {...settings}>
+        {TagsData.map((tag) => (
+          <HeaderTagItem key={tag.text} text={tag.text} img={tag.img} />
+        ))}
+      </StyledSlider>
     </>
   )
 }
+
+const settings = {
+  dots: false,
+  infinite: false,
+  speed: 500,
+  slidesToShow: 10,
+  slidesToScroll: 6,
+  // variableWidth: true,
+}
+
+const StyledSlider = styled(Slider)``
 
 export default HeaderTags
