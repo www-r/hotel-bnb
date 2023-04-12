402395fb7a75a26cd431486cb7c582595340a528
bwlee <lbw3973@naver.com>
2023-04-12 19:34:56 +0900

    Feat: Tag검색 구현 완료

--- src/components/Common/Tags/TagItem/TagItem.jsx
+++ src/components/Common/Tags/TagItem/TagItem.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import * as S from './TagItem.style'
+import { useNavigate } from 'react-router-dom'
+import { RoomsContext } from '@/contexts/RoomsProvider'
 
 const HeaderTagItem = ({ text, img }) => {
+  const { rooms } = useContext(RoomsContext)
+
+  const navigate = useNavigate()
+  const handleClick = () => {
+    const res = rooms.filter((room) => room.tags.includes(text.replace(/ /g, '')))
+    navigate(`/search/${text}`, {
+      state: { res },
+    })
+  }
   return (
-    <S.Container>
+    <S.Container onClick={handleClick}>
       <S.ImageContainer src={img} />
       <S.SpanContent>{text}</S.SpanContent>
     </S.Container>
