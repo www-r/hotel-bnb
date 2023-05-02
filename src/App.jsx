86bef66916dbe1c2208219055a6a48051a082722
dokimion24 <leecy3610@gmail.com>
2023-05-02 12:06:39 +0900

    feat: queryProvider, queryDevtools 추가

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,23 @@ import Router from '@/routes/Router'
 import RoomsProvider from './contexts/RoomsProvider'
 import UserProvider from './contexts/UserProvider'
 import LoginProvider from './contexts/LoginProvider'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+
+const queryClient = new QueryClient()
 
 function App() {
   return (
-    <LoginProvider>
-      <UserProvider>
-        <RoomsProvider>
-          <Router />
-        </RoomsProvider>
-      </UserProvider>
-    </LoginProvider>
+    <QueryClientProvider client={queryClient}>
+      <LoginProvider>
+        <UserProvider>
+          <RoomsProvider>
+            <Router />
+            <ReactQueryDevtools />
+          </RoomsProvider>
+        </UserProvider>
+      </LoginProvider>
+    </QueryClientProvider>
   )
 }
 
