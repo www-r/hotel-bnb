0cb0aaf768d3d4cf1ee58c36420e3a24bb9085fc
dokimion24 <leecy3610@gmail.com>
2023-05-02 12:08:09 +0900

    feat: 에러, 로딩 부분 제거

--- src/components/Room/RoomList/RoomList.jsx
+++ src/components/Room/RoomList/RoomList.jsx
@@ -3,10 +3,7 @@ import RoomItem from '@/components/Room/RoomItem/RoomItem'
 
 import * as S from '@/components/Room/RoomList/RoomList.style'
 
-const RoomList = ({ rooms, loading, error }) => {
-  if (loading) return <div>loading...</div>
-  if (error) return <div>에러가 발생</div>
-
+const RoomList = ({ rooms }) => {
   return (
     <S.Main>
       <S.Container>
