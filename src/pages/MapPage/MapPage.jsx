1794627db3edcb62500946c4331595e129f3837d
dokimion24 <leecy3610@gmail.com>
2023-05-02 12:05:41 +0900

    feat: context에서 관리하던 rooms 데이터 useQuery로 변경  # Please enter the commit message for your changes. Lines starting

--- src/pages/MapPage/MapPage.jsx
+++ src/pages/MapPage/MapPage.jsx
@@ -1,15 +1,20 @@
-import React, { useState, useContext } from 'react'
-import { RoomsContext } from '@/contexts/RoomsProvider'
+import React, { useState } from 'react'
 
 import MapList from '@/components/Map/MapList/MapList'
 import Header from '@/components/Common/Header/Header'
 import MapButton from '@/components/Map/MapButton/MapButton'
 
 import '@/components/Map/map.css'
+import { useQueryClient } from 'react-query'
+import { queryKeys } from '@/utils/queryKeys'
+import { useGetRooms } from '../../hooks/useGetRooms'
 
 const MapPage = () => {
-  const roomsCtx = useContext(RoomsContext)
-  const { rooms, loading, error } = roomsCtx
+  console.log('...rendering')
+  // const queryClient = useQueryClient()
+  // const rooms = queryClient.getQueryData(queryKeys.rooms)
+  const { rooms } = useGetRooms()
+
   const [movePage, setMovePage] = useState('/')
 
   return (
