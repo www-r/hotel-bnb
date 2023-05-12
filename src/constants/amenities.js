982fa8e59a0621b1d5769ef2ed6df86ce51b69d5
youngeun <jessicak0828@gmail.com>
2023-05-12 17:55:18 +0900

    feat: assets 에 필요한 images 추가

---
+++ src/constants/amenities.js
@@ -0,0 +1,54 @@
+export const AmenitiesData = [
+  {
+    img: '/src/assets/images/AmenitiesIcons/Cooler.svg',
+    text: '냉방'
+  },
+  {
+    img: '/src/assets/images/AmenitiesIcons/HairDryer.svg',
+    text: '헤어드라이기'
+  },
+  {
+    img: '/src/assets/images/AmenitiesIcons/Heater.svg',
+    text: '난방'
+  },
+  {
+    img:'/src/assets/images/AmenitiesIcons/Kitchen.svg',
+    text:'주방'
+  },
+  {
+    img:'/src/assets/images/AmenitiesIcons/Parking.svg',
+    text: '무료주차'
+  },
+  {
+    img:'/src/assets/images/AmenitiesIcons/Television.svg',
+    text:'티비'
+  },
+  {
+    img:'/src/assets/images/AmenitiesIcons/Wifi.svg',
+    text:'와이파이'
+  },
+  {
+    img:'/src/assets/images/AmenitiesIcons/MicrowaveOven.svg',
+    text:'전자레인지'
+  },
+  {
+    img:'/src/assets/images/AmenitiesIcons/Dinings.svg',
+    text:'식기류'
+  },
+  {
+    img:'/src/assets/images/AmenitiesIcons/Beddings.svg',
+    text:'침구류'
+  },
+  {
+    img:'/src/assets/images/AmenitiesIcons/netflix.svg',
+    text:'넷플릭스'
+  },
+  {
+    img:'/src/assets/images/AmenitiesIcons/garden.svg',
+    text:'마당'
+  },
+  {
+    img:'/src/assets/images/AmenitiesIcons/Amenities.svg',
+    text:'생필품'
+  }
+]
