560e37e5b932b350fc18750ef0c90ffff0fa6f2d
youngeun <jessicak0828@gmail.com>
2023-05-14 04:37:17 +0900

    feat: Header에 있는 tags 부분 스크롤 내릴 때 사라지게 하기

--- src/components/Common/Header/Header.jsx
+++ src/components/Common/Header/Header.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react'
 import HeaderMenu from './HeaderMenu/HeaderMenu'
 import * as S from './Header.style'
 import HeaderSearch from '@/components/Common/Header/HeaderSearch/HeaderSearch'
@@ -5,6 +6,22 @@ import HeaderTags from '@/components/Common/Tags/TagList'
 import Logo from '@/components/Common/Logo'
 
 const Header = () => {
+  const [downward, setDownward] = useState(false)
+  // this variable is used to store the last scroll position
+  window.addEventListener(
+    'scroll',
+    function () {
+      console.log(scrollY)
+      // on scroll,
+      if (scrollY < 50) {
+        setDownward(false)
+      } else if (scrollY > 50) {
+        setDownward(true)
+      }
+      //lastScrollTop = scrollTop // update the last scroll position to the current one
+    },
+    false,
+  )
   return (
     <S.Header>
       <S.Container>
@@ -12,9 +29,13 @@ const Header = () => {
         <HeaderSearch />
         <HeaderMenu />
       </S.Container>
-      <S.TagItemsContatiner>
-        <HeaderTags />
-      </S.TagItemsContatiner>
+      {downward ? (
+        <></>
+      ) : (
+        <S.TagItemsContatiner>
+          <HeaderTags />
+        </S.TagItemsContatiner>
+      )}
     </S.Header>
   )
 }
